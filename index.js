47acc2eab9406b9d4eb4d373538d4aa04ddfba9c
NajeebSultan <najeebsultan070@gmail.com>
2025-02-24 15:54:38 +0500

    javascript file of program counter

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ console.log(result);
 let username;
 //username = window.prompt(`Whats your name?`)
 //console.log(username);
-/*
-document.getElementById("mysubmit").onclick = function(){
+
+//document.getElementById("mysubmit").onclick = function(){
   username = document.getElementById("mytext").value;
   document.getElementById("myh1").textContent = `Hello ${username}`
-}
-*/
+// }
+
 
 
 /* var age = window.prompt('How old are you');
@@ -42,10 +42,19 @@ console.log(age);
     let pi= 3.14;
     let radius;
     let circumference;
-/*
+
+   /* 
     radius = window.prompt('Enter the radius of circle');
     radius= Number(radius);
 
     circumference= 2 * pi *radius;
-    console.log(circumference);
-*/
+    console.log(circumference); 
+    */
+  
+
+    document.getElementById("mysubmit").onclick = function(){
+      radius = document.getElementById("mytext").value;
+      radius = Number(radius);
+      circumference = 2 * pi * radius;
+      document.getElementById("myh3").textContent = circumference + "cm";
+    }
