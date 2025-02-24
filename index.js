f3eee5f028966f12ae61e7e74c178e05229fea47
NajeebSultan <najeebsultan070@gmail.com>
2025-02-24 16:23:14 +0500

    Learn the interaction of math in js

--- index.js
+++ index.js
@@ -60,10 +60,20 @@ console.log(age);
     }
 
 
- let x =3.99;
+ let x =3;
 let y =2;
-let z;
+let z = 1;
 
-z=Math.round(x);
+//z=Math.round(x);
+//z=Math.floor(x);
+//z=Math.ceil(x);
+//z=Math.trunc(x);
+//z=Math.pow(x , y);
+//z=Math.sqrt(x);
+//z=Math.sin(x);
 
-console.log(z);
+let max = Math.max(x , y , z);
+let min = Math.min(x , y , z);
+
+console.log(max);
+console.log(min);
--- math object.js
+++
@@ -1,7 +0,0 @@
-let x =3.99;
-let y =2;
-let z;
-
-z=Math.round(x);
-
-console.log(z);
