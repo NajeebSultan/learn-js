cd178e76e3ff888c7a61f2c52956d8378cf6dd12
NajeebSultan <najeebsultan070@gmail.com>
2025-02-24 14:50:37 +0500

    Learn constants

--- index.js
+++ index.js
@@ -23,16 +23,29 @@ console.log(result);
 let username;
 //username = window.prompt(`Whats your name?`)
 //console.log(username);
-
+/*
 document.getElementById("mysubmit").onclick = function(){
   username = document.getElementById("mytext").value;
   document.getElementById("myh1").textContent = `Hello ${username}`
 }
+*/
 
 
-
-var age = window.prompt('How old are you');
+/* var age = window.prompt('How old are you');
 age=Number(age)+1;
 console.log(age);
-    
+ */
 
+    
+    //const = values that cannot be changed.
+
+    let pi= 3.14;
+    let radius;
+    let circumference;
+/*
+    radius = window.prompt('Enter the radius of circle');
+    radius= Number(radius);
+
+    circumference= 2 * pi *radius;
+    console.log(circumference);
+*/
