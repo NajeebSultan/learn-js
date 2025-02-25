300ba9026d851617450df72978c8ff30a8130feb
NajeebSultan <najeebsultan070@gmail.com>
2025-02-25 23:36:00 +0500

    learn switch statment

--- index.js
+++ index.js
@@ -115,3 +115,33 @@ console.log(greeting);
 let isstudent = true;
 let alert = isstudent ? "You are student" : "You are NOT a student"
 console.log(alert);
+
+// switch statement
+
+let day =5;
+
+switch(day){
+  case 1:
+  console.log("It is monday");
+  break;
+  case 2:
+  console.log("It is tuesday");
+  break;
+  case 3:
+  console.log("It is Wednesday");
+  break;
+  case 4:
+  console.log("It is Thursday");
+  break;
+  case 5:
+  console.log("It is Friday");
+  break;
+  case 6:
+  console.log("It is Saturday");
+  break;
+  case 7:
+  console.log("It is Sunday");
+  break;
+  default:
+  console.log("enter a valid input"); 
+}
