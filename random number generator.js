bfecfa151c4197bc0c3c0489be9fee9cb9ead72e
NajeebSultan <najeebsultan070@gmail.com>
2025-02-25 17:08:11 +0500

    javascript file is done of random number generator

--- random number generator.js
+++ random number generator.js
@@ -6,6 +6,6 @@ const max = 6;
 let randomNum;
 
 mybutton.onclick = function(){
-   randomNum = Math.floor(Math.random * max) + min;
+   randomNum = Math.floor(Math.random() * max) + min;
    myLabel.textContent = randomNum;
 }
