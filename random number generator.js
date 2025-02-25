df3da466777d1c1a05d5e58298977c4fb2f19963
NajeebSultan <najeebsultan070@gmail.com>
2025-02-25 17:16:50 +0500

    Add more labels in js

--- random number generator.js
+++ random number generator.js
@@ -1,6 +1,8 @@
 
 const mybutton = document.getElementById("mybutton");
-const myLabel = document.getElementById("myLabel");
+const Label1 = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
 const min = 1;
 const max = 6;
 let randomNum;
