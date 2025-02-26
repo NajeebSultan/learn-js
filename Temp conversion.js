8f565cfc574efcc556b43524884399d559dd4293
NajeebSultan <najeebsultan070@gmail.com>
2025-02-26 23:56:43 +0500

    Const the variables names of units

---
+++ Temp conversion.js
@@ -0,0 +1,9 @@
+const textBox = document.getElementById("textBox");
+const  toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsis = document.getElementById("toCelsis");
+const result = document.getElementById("result");
+let temp;
+
+funtion Convert(){
+  
+}
