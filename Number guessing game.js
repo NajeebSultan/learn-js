35191bf8d407d2cdff45d3ad5049a0a0e70fb4d7
NajeebSultan <najeebsultan070@gmail.com>
2025-02-26 22:11:09 +0500

    Making window prompt and alert for taking a numbers from users

--- Number guessing game.js
+++ Number guessing game.js
@@ -2,3 +2,16 @@ const minNum = 1;
 const maxNum = 100;
 
 const answer = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+
+while(running){
+  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+  guess = Number(guess);
+  console.log(typeof guess , guess);
+  running = false;
+  
+}
