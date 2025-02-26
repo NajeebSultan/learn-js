86560118384ff4a50fcffaebd68651f0b4d8077f
NajeebSultan <najeebsultan070@gmail.com>
2025-02-27 00:15:26 +0500

    Write a formula of convert celsius to farehheit

--- Temp conversion.js
+++ Temp conversion.js
@@ -1,9 +1,21 @@
 const textBox = document.getElementById("textBox");
 const  toFahrenheit = document.getElementById("toFahrenheit");
-const toCelsis = document.getElementById("toCelsis");
+const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
 let temp;
 
-funtion Convert(){
-  
+function convert(){
+
+  if(toFahrenheit.checked){
+    temp = Number(textBox.value);
+    temp = temp * 9 /5 +32 ;
+    result.textContent = temp.toFixed(1)+ "°F";
+  }
+  else if(toCelsius.checked){
+    result.textContent = "You selected to celsius";
+    
+  }
+  else{
+    result.textContent = "Select a unit";
+  }
 }
