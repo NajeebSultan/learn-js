fe6094f8abd67da2c6a74bda458b59e58aaef922
NajeebSultan <najeebsultan070@gmail.com>
2025-02-27 22:26:48 +0500

    make a sum program with function and rest parameters

--- index.js
+++ index.js
@@ -301,3 +301,36 @@ let name = "Najeeb Sultan";
 let letters = [...name].join("-");
 console.log(letters);
 
+
+// rest parameters
+
+function openFridge(...foods){
+  console.log(...foods);
+}
+
+function getFood(...foods){
+  return foods;
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+
+openFridge(food1 , food2 , food3 , food4);
+
+
+function sum(...numbers){
+
+  let result = 0;
+  for( let number of numbers){
+    result += number;
+  }
+  return result;
+}
+
+const total = sum (1, 2, 3, 4);
+
+console.log(`your total is $${total}`);
+
+
