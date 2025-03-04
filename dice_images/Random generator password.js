afb8bc3c89b80b47b30ad8b692e414e4c9a3ce66
NajeebSultan <najeebsultan070@gmail.com>
2025-03-04 23:44:53 +0500

    initialize some variables

---
+++ dice_images/Random generator password.js
@@ -0,0 +1,28 @@
+
+function generatePassword(length ,includeLowercase , includeUppercase , includeNumber ,includeSymbols ){
+
+const lowercaseChars = "abcdefghigklmnopqrstuvwxyz";
+const uppercaseChars ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolsChars = "!@#$%^&*()_+-="; 
+
+let allowedChars = "";
+let password = "";
+
+  return '';
+}
+
+
+
+const passwordLength = 12;
+const includeLowercase = true ;
+const includeUppercase = true ;
+const includeNumber = true ;
+const includeSymbols = true ;
+
+ const password = generatePassword(PasswordLength ,includeLowercase , includeUppercase , includeNumber ,includeSymbols );
+ console.log('Generated password: ${password}');
+  
+
+
+
