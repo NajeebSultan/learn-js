142fff757b71ff92f8f1655a1332f1d7c6ce26a5
NajeebSultan <najeebsultan070@gmail.com>
2025-03-07 22:13:47 +0500

    add h1 heading and id is my h1

--- callback function.js
+++ callback function.js
@@ -16,3 +16,14 @@ function leave(){
 function goodbye(){
   console.log("Goodbye!");
 }
+
+sum(displayConsole , 1 ,2);
+
+function sum(callback , x ,y){
+  let result = x + y;
+  callback(result);
+}
+
+function displayConsole(result){
+  console.log(result);
+}
