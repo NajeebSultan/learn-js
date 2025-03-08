ee00dea4b18d978693a86690d9c426aa8c90a9d1
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 16:17:48 +0500

    Make objects of different attribute

---
+++ objects.js
@@ -0,0 +1,26 @@
+
+const person1 = {
+  firstName: "Najeeb",
+  lastName: "Sultan",
+  age:18,
+  isEmployed: true,
+}
+
+const person2 = {
+  firstName: "Sameed",
+  lastName: "Ullah",
+  age:25,
+  isEmployed: true,
+}
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+
+
