3ea053beb22c7254fac822c8dc093f0172ae475e
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 17:30:01 +0500

    Make html file of class

---
+++ class.js
@@ -0,0 +1,17 @@
+class Product{
+  constructor(name ,price){
+    this.name = name;
+    this.price = price;
+  }
+  displayProduct(){
+    console.log(`product: ${this.name}`);
+    console.log(`Price: $${this.price.toFixed(2)}`);
+
+  }
+}
+
+const product1 = new Product("Shirt",19.99);
+const product2 = new Product("Pants",22.50);
+const product3 = new Product("Underwear",100.00);
+
+product3.displayProduct();
