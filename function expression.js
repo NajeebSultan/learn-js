c5e3f8ad1a3c430f3ae87d26d5ec891d366824b8
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 15:21:59 +0500

    Learn function declaration

--- function expression.js
+++ function expression.js
@@ -2,3 +2,16 @@ setTimeout(function(){
   console.log("Hello");
 
 }, 3000);
+
+const numbers = [1 , 2 , 3 ,4 ,5 ,6];
+const squares = numbers.map(square);
+
+console.log(squares);
+
+
+function square(element){
+  return Math.pow(element ,2);
+}
+
+
+
