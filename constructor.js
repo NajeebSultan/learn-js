49c6e1fd1cba73f1dadc1846299ea2bd6c58e1be
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 16:51:12 +0500

    Write car constructor formatting for better readability and consistency

---
+++ constructor.js
@@ -0,0 +1,21 @@
+function Car(make , model , year , color){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+}
+
+const car1 = new Car("ford", "Mustang" , 2024 , "red");
+const car2 = new Car("Chevrolet", "camaro" , 2025 , "blue");
+
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.color);
+
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
+
+
