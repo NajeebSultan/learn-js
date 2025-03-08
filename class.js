dc858452f89cd6b56f66b4c3f0990e90c1dc0504
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 17:37:45 +0500

    Add total price of product and tax by using function

--- class.js
+++ class.js
@@ -8,10 +8,17 @@ class Product{
     console.log(`Price: $${this.price.toFixed(2)}`);
 
   }
+
+  calculateTotal(salesTax){
+    return this.price + (this.price * salesTax);
+  }
 }
+const salesTax = 0.05;
 
 const product1 = new Product("Shirt",19.99);
 const product2 = new Product("Pants",22.50);
 const product3 = new Product("Underwear",100.00);
 
 product2.displayProduct();
+const total = product1.calculateTotal(salesTax);
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
