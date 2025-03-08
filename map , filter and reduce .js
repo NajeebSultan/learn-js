9f85286986534618dd3e5dcd6bc19aebcca3702d
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 15:04:05 +0500

    Learn the concept of reduce i max and min numbers

--- map , filter and reduce .js
+++ map , filter and reduce .js
@@ -73,3 +73,20 @@ function sum (accumulator , element){
 }
 
 
+const grades = [75 , 50 , 60 , 65 , 70];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(accumulator , element){
+  return Math.max(accumulator , element);
+}
+
+function getMin(accumulator , element){
+  return Math.min(accumulator , element);
+}
+
+
