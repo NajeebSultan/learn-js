bd86a9c510161f6e173f924f4345ec1b54c8a6ba
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 16:55:26 +0500

    Add one more attribute in car constructor

--- constructor.js
+++ constructor.js
@@ -3,6 +3,7 @@ function Car(make , model , year , color){
   this.model = model;
   this.year = year;
   this.color = color;
+  this.drive = function(){console.log(`You drive the ${this.model}`)}
 }
 
 const car1 = new Car("ford", "Mustang" , 2024 , "red");
@@ -12,10 +13,13 @@ console.log(car1.make);
 console.log(car1.model);
 console.log(car1.year);
 console.log(car1.color);
+car1.drive();
+
 
 console.log(car2.make);
 console.log(car2.model);
 console.log(car2.year);
 console.log(car2.color);
+car2.drive();
 
 
