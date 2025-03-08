712a18f6ab5c8c57808b8aca170e09908833787c
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 15:34:28 +0500

    Learn function expression , taking square and cube and even or odd and total function by using array

--- function expression.js
+++ function expression.js
@@ -19,13 +19,17 @@ const evenNums = numbers.filter(function(element){
 const oddNums = numbers.filter(function(element){
   return element % 2 !==0;
 });
+ 
+const total = numbers.reduce(function(accumulator , element){
+  return accumulator + element;
+});
 
 
 console.log(squares);
 console.log(cubes);
 console.log(evenNums);
 console.log(oddNums);
-
+console.log(total);
 
 
 
