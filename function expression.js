172ddce23334e5c304ff316e458d43896ed9a6fa
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 15:27:44 +0500

    Learn function expression , taking square and cube by using array

--- function expression.js
+++ function expression.js
@@ -3,15 +3,20 @@ setTimeout(function(){
 
 }, 3000);
 
-const numbers = [1 , 2 , 3 ,4 ,5 ,6];
-const squares = numbers.map(square);
+const numbers = [1 , 2 , 3 , 4 , 5 , 6];
+const squares = numbers.map(function square(element){
+  return Math.pow(element ,2);
+});
+
+const cubes = numbers.map(function square(element){
+  return Math.pow(element ,3);
+});
 
 console.log(squares);
+console.log(cubes);
 
 
-function square(element){
-  return Math.pow(element ,2);
-}
+
 
 
 
