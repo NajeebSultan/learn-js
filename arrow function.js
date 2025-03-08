efe0d9bc4e565e217c3620380c07355f19844c82
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 16:01:58 +0500

    Learn the concept of arrow function to display square and cube of numbers

--- arrow function.js
+++ arrow function.js
@@ -5,3 +5,12 @@ hello("Bro" , 25);
 
 
 setTimeout( () => console.log("Hello") ,3000);
+
+const numbers = [ 1, 2, 3, 4, 5, 6];
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+
+console.log(cubes);
+console.log(squares);
+
+
