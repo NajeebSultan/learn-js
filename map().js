fd302270d8cf80f2c5930399a033a422d92f74b7
NajeebSultan <najeebsultan070@gmail.com>
2025-03-08 14:19:48 +0500

    Write uppercase and lowercase function using map

--- map().js
+++ map().js
@@ -1,11 +1,19 @@
 const students = ["ali , ahmed , hassan "]
 const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
 
 console.log(studentsUpper);
+console.log(studentsLower);
 
 function upperCase(element){
   return element.toUpperCase();
 }
+
+function lowerCase(element){
+  return element.toLowerCase();
+}
+
 
 
 
