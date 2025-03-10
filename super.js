8366a07c429ea5e421336fc1683559f83362fecb
NajeebSultan <najeebsultan070@gmail.com>
2025-03-10 23:30:30 +0500

    Make different classes

---
+++ super.js
@@ -0,0 +1,28 @@
+class Animal{
+
+  constructor(){
+
+  }
+}
+class Rabbit extends Aniamls{
+
+   constructor(name , age , runspeed) {
+    this.name = name;
+    this.age = age;
+    this.runspeed = runspeed;
+   } 
+}
+class Fish extends Aniamls{
+  constructor(name , age , swimspeed) {
+    this.name = name;
+    this.age = age;
+    this.swimspeed = swimspeed;
+   } 
+}
+class Hawk extends Aniamls{
+  constructor(name , age , flyspeed) {
+    this.name = name;
+    this.age = age;
+    this.flyspeed = flyspeed;
+   } 
+}
