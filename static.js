1ea28192e017a6fa40090437c91ad2865ae2044b
NajeebSultan <najeebsultan070@gmail.com>
2025-03-10 21:46:31 +0500

    Find Diameter and circumference by static method

--- static.js
+++ static.js
@@ -1,4 +1,13 @@
 class MathUtil{
   static PI = 3.14159;
+
+  static getDiameter(radius){
+    return radius * 2;
+  }
+  static getCircumference(radius){
+    return 2 * this.PI *radius;
+  }
 }
-console.log(MathUtil.PI)
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
