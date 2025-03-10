00bec254d1b18cc50a5ed687c7df55c8f3ec51ca
NajeebSultan <najeebsultan070@gmail.com>
2025-03-10 23:41:37 +0500

    Learn the concept of super in parent and child class

--- super.js
+++ super.js
@@ -1,28 +1,37 @@
-class Animal{
+class Animals{
 
-  constructor(){
-
-  }
-}
-class Rabbit extends Aniamls{
-
-   constructor(name , age , runspeed) {
+  constructor(name , age){
     this.name = name;
     this.age = age;
+  }
+}
+class Rabbit extends Animals{
+
+   constructor(name , age , runspeed) {
+    super(name , age);
     this.runspeed = runspeed;
    } 
 }
-class Fish extends Aniamls{
+class Fish extends Animals{
   constructor(name , age , swimspeed) {
-    this.name = name;
-    this.age = age;
+    super(name , age);
     this.swimspeed = swimspeed;
    } 
 }
-class Hawk extends Aniamls{
+class Hawk extends Animals{
   constructor(name , age , flyspeed) {
-    this.name = name;
-    this.age = age;
+    super(name , age);
     this.flyspeed = flyspeed;
    } 
 }
+
+const rabbit = new Rabbit("rabbit" , 1 ,25);
+const fish = new Fish("fish" , 2 ,12);
+const hawk = new Hawk("hawk" , 3 ,50);
+
+console.log(rabbit.name);
+console.log(rabbit.age);
+console.log(rabbit.runspeed);
+console.log(fish.name);
+console.log(fish.age);
+console.log(fish.swimspeed);
