5ef7aa03a7df9b3238f78436d3e31fe7b272de5c
NajeebSultan <najeebsultan070@gmail.com>
2025-03-11 23:01:54 +0500

    Make a set funtion of width and make conditional statements

--- getter and setter.js
+++ getter and setter.js
@@ -4,6 +4,14 @@ class Rectangle {
     this.height = height;
 
   }
+  set width(newWidth){
+    if(newWidth > 0){
+      this.width = newWidth;
+    }
+    else{
+      console.log("Width must be a positive number")
+    }
+  }
 }
 const rectangle = new Rectangle(-1000000 , "pizza");
 console.log(rectangle.width);
