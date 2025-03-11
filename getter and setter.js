8c8fe3101a4bb955c00a7d565e661b4c28388214
NajeebSultan <najeebsultan070@gmail.com>
2025-03-11 23:30:29 +0500

    Make a new class of person

--- getter and setter.js
+++ getter and setter.js
@@ -42,3 +42,16 @@ console.log(rectangle.width);
 console.log(rectangle.height);
 console.log(rectangle.area);
 
+
+class Person{
+  constructor(firstName , lastName , age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+const person = new Person(420 , 69 ,"pizza");
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+
