d51c8a8c21d54d9132b88f1335998e3976e3d357
NajeebSultan <najeebsultan070@gmail.com>
2025-03-14 00:09:52 +0500

    Display info of person

--- nested objects.js
+++ nested objects.js
@@ -10,3 +10,8 @@ const person = {
   }
 }
 
+console.log(person.fullName);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.hobbies[2]);
+console.log(person.address.city);
