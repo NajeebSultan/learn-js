92c12736a26f5f2df673dabc1d58b4937dc639c1
NajeebSultan <najeebsultan070@gmail.com>
2025-03-14 00:14:26 +0500

    Make a class of person

--- nested objects.js
+++ nested objects.js
@@ -18,7 +18,13 @@ console.log(person.address.city);
 
 
 
-
+class Person{
+  constructor(name , age , ...address){
+    this.name = name;
+    this.age = age;
+    this.address = new Address(...address);
+  }
+}
 class Address{
   constructor(street , city , country){
     this.street = street;
