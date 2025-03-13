b1f6b51110a8cda3eab28fcbda6f4b0e2f86f178
NajeebSultan <najeebsultan070@gmail.com>
2025-03-14 00:46:32 +0500

    make an array of objects

--- array objects.js
+++ array objects.js
@@ -2,4 +2,6 @@ const fruits = [{name: "apple" , color: "red" , calories: 45},
   {name: "orange" , color: "oange" , calories: 45},
   {name: "Banana" , color: "yellow" , calories: 105},
   {name: "coconut" , color: "white" , calories: 98},
-  {name: "pineapplr" , color: "yellow" , calories: 37}];
+  {name: "pineapple" , color: "yellow" , calories: 37}];
+
+  console.log(fruits[4].name);
