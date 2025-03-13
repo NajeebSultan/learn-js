d7eb44bc4203ba479bf604ef6928f9be8d72d57a
NajeebSultan <najeebsultan070@gmail.com>
2025-03-14 00:56:22 +0500

    Write map method to display the colors of fruits

--- array objects.js
+++ array objects.js
@@ -5,4 +5,15 @@ const fruits = [{name: "apple" , color: "red" , calories: 45},
   {name: "pineapple" , color: "yellow" , calories: 37}];
 
   console.log(fruits);
+// forEach method
+
   fruits.forEach(fruit => console.log(fruit.color));
+// map method
+  const fruitName = fruits.map(fruit => fruit.name);
+  const fruitColors = fruits.map(fruit => fruit.color);
+  const fruitcalories= fruits.map(fruit => fruit.calories);
+
+  console.log(fruitName);
+  console.log(fruitColors);
+  console.log(fruitcalories);
+
