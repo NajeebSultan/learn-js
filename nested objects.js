dcb266d9ab533b1937eb7dd2a8e5419caf1a0ec4
NajeebSultan <najeebsultan070@gmail.com>
2025-03-14 00:06:45 +0500

    Make attribtes of person in class

---
+++ nested objects.js
@@ -0,0 +1,12 @@
+const person = {
+  fullName: "Najeeb sultan",
+  age: 18,
+  isStudent: true,
+  hobbies: ["karate" , "Horse riding ", "cooking"],
+  address: {
+    street: "124 conch st.",
+    city: "Islamabad",
+    country: "Pakistan"
+  }
+}
+
