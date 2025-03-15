94596d0ca1529dd091f60e50dc52750156b92b1f
NajeebSultan <najeebsultan070@gmail.com>
2025-03-15 14:32:49 +0500

    Learn the concept of closure and increment the numbers

--- closure.js
+++ closure.js
@@ -8,10 +8,18 @@ console.log(message);
 }
 outer();
 
+function createCounter(){
 
-let count = 0;
-function increment(){
   let count = 0;
+
+function increment(){
+   
   count++;
   console.log(`Count increased to ${count}`);
 }
+return {increment};
+
+}
+const counter = createCounter();
+counter.increment();
+counter.increment();
