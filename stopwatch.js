2145e03a15b6c197dabd16d6a2465d4663e443ad
NajeebSultan <najeebsultan070@gmail.com>
2025-03-20 00:38:59 +0500

    Write formulas for hours , min , and sec , millisec

--- stopwatch.js
+++ stopwatch.js
@@ -25,4 +25,11 @@ function reset(){
 
 function update(){
   
+  const currentTime  = Date.now();
+  elapsedTime = currentTime - startTime;
+
+  let hours = Math.floor(elapsedTime / (1000* 60 * 60));
+  let minutes = Math.floor(elapsedTime / (1000* 60 ) % 60);
+  let seconds = Math.floor(elapsedTime / (1000 % 60));
+  let milliseconds = Math.floor(elapsedTime % 1000 / 10);
 }
