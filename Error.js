06550e96850946a4618d834024af204fcda552a1
NajeebSultan <najeebsultan070@gmail.com>
2025-03-20 23:49:25 +0500

    Learn concept of try and catch

--- Error.js
+++ Error.js
@@ -2,6 +2,14 @@ try{
   const dividend = window.prompt("Enter a dividend: ");
   const divisor = window.prompt("Enter a divisor: ");
   
+if(divisor == 0){
+  throw new Error("You cannot divide by zero");
+
+}
+if (isNaN(dividend) || isNaN(divisor)){
+  throw new Error("Values must be a numbers");
+}
+
   const result = dividend / divisor;
   console.log(result);
 }
