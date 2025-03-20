4f7abfe1280a50e82a0834de796cfd9f335d09a3
NajeebSultan <najeebsultan070@gmail.com>
2025-03-20 23:45:08 +0500

    Learn concept of try and catch

--- Error.js
+++ Error.js
@@ -1,5 +1,11 @@
-const dividend = window.prompt("Enter a dividend: ");
-const divisor = window.prompt("Enter a divisor: ");
-
-const result = dividend / divisor;
-console.log(result);
+try{
+  const dividend = window.prompt("Enter a dividend: ");
+  const divisor = window.prompt("Enter a divisor: ");
+  
+  const result = dividend / divisor;
+  console.log(result);
+}
+catch(error){
+  console.log(error);
+}
+console.log("You have reached the end! ");
