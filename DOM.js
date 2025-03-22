1ced28bf0a3611ac46123ab2b88d604f61df437f
NajeebSultan <najeebsultan070@gmail.com>
2025-03-22 23:21:23 +0500

    Write welcome username or guest using DOM

--- DOM.js
+++ DOM.js
@@ -1,4 +1,9 @@
 console.dir(document);
 document.title = "My website";
 
-document.body.style.background = "black" ;
+//document.body.style.background = "black" ;
+
+const username = "Najeeb";
+const welcomeMsg = document.getElementById("welcome-msg");
+
+welcomeMsg.textContent += username === " " ? ` Guest` :  username;
