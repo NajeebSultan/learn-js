50ba3b6ad7d358dd489a30a976b60df21278087c
NajeebSultan <najeebsultan070@gmail.com>
2025-03-23 15:05:07 +0500

    Append the box in a box1

--- add_change_html.js
+++ add_change_html.js
@@ -3,5 +3,7 @@ const newH1 = document.createElement("h1");
 newH1.textContent = "I like pizza!";
 newH1.id = "myH1";
 newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
 
-document.body.prepend(newH1);
+document.getElementById("box1").append(newH1);
+//document.body.prepend(newH1);
