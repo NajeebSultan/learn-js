b1f193de6b921d3e8c26709bcfc69d9d9bb6a225
NajeebSultan <najeebsultan070@gmail.com>
2025-03-27 23:25:04 +0500

    Prepend the heading inside the box

--- add_change_html.js
+++ add_change_html.js
@@ -5,6 +5,6 @@ newH1.id = "myH1";
 newH1.style.color = "tomato";
 newH1.style.textAlign = "center";
 
-//document.getElementById("box1").append(newH1);
-//document.body.prepend(newH1);
+document.getElementById("box1").append(newH1);
+document.body.prepend(newH1);
 document.getElementById("box1").prepend(newH1);
